1f798f2b9aa990f177ab7de0f233e198c08a77f4
Marco Chiti <marco.chiti@topfinish.it>
2020-07-09 16:37:59 +0200

    barcode ordini

---
+++ Applicazioni/Applicazioni.Models/BarcodeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applicazioni.Models
+{
+    public class BarcodeModel
+    {
+        public string Barcode;
+        public string Nummovfase;
+        public string Reparto;
+        public string Quantità;
+    }
+}
